807c3e98fa874b49d381b932c8edd1e52a188189
Maria Deyneka <m.deyneka@yahoo.com>
2024-01-30 14:34:43 +0200

    fixes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 if (window.innerWidth <= 900) {
   const imageContainer = document.getElementById("smallImage");
   const img = document.createElement("img");
-  img.src = "images/logo_circle.svg";
+  img.src = "images/logo_big.svg";
   img.alt = "logo_circle";
   img.className = "logo";
   imageContainer.appendChild(img);
